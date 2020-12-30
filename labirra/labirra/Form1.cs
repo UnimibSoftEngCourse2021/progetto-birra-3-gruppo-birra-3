198c7c35ecab5e07532cc5b7d910a466317ac3af
Rriva36 <75070326+Rriva36@users.noreply.github.com>
2020-12-30 16:37:34 +0100

    parte 1 codice c#
    con sto coso ci si connette al database

---
+++ labirra/labirra/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labirra
+{
+    public partial class Form1 : Form
+    {
+        private gestionedb database;
+
+        public Form1()
+        {
+            InitializeComponent();
+            database = new gestionedb();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            database.create_table();
+            MessageBox.Show("fatto");
+
+        }
+    }
+}
